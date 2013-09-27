fcea2b493aa02e22ba93b57c82f60fa5b8d8f439
Rainbough Phillips <rainbough@gmail.com>
2013-09-27 13:47:34 -0500

    create initial rails application

---
+++ urdb/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urdb::Application.config.secret_key_base = 'ec67fc3e3b653daeb619ae3345a252be2aaad3fa8382b5a14a2d079dc301f2a644749fec90715d8cd2617f8cd5cfbe5e2eebb054586a50260cbeb47bfa812c65'
